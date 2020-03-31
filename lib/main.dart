2d1c2134fc4580637056e8054a0007bc0404ec9b
dev-marceloborba <marceloborba@MacBook-Pro-de-Quantico.local>
2020-03-31 14:29:13 -0300

    added theme config

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+
       ),
       home: ProductsOverviewScreen(),
       debugShowCheckedModeBanner: false,
